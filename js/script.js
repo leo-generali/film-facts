c0b8fa8cb96c4afcab279524a2f5ff7a5fe9b333
Leo Generali <leogen17@gmail.com>
2017-02-13 22:13:50 -0500

    Add API call

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
-model = {};
 
-view = {};
+model = {
+  searchMovie: function(){
+    var httpRequest = new XMLHttpRequest();
+    var movie = document.getElementById('searchbar').value;
+    console.log(movie);
+    var url = 'http://www.omdbapi.com/?t=' + movie //+ '&y=&plot=short&r=json';
+
+    httpRequest.onreadystatechange = function(){
+      if(this.readyState === 4){
+        console.log('good');
+        var filmInfo = JSON.parse(this.responseText);
+        view.updateFilmObj(filmInfo);
+      }else{
+        console.log('bad');
+      }
+    };
+    httpRequest.open('GET', url, true);
+    httpRequest.send(null); 
+    return false;  
+  },
+
+
+};
+
+view = {
+  init: function(){
+    document.getElementById('searchbar-go').addEventListener('click', model.seachMovie);
+  },
+
+  updateFilmObj: function(obj){
+    document.getElementById('title').innerHTML = obj.Title;
+  }
+};
 
 controller = {};
+
+
+
+view.init();
